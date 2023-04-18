7aed6778782f539cf8efe23ec149b388b418dffb
Mikael Andri Ingason <33830730+MikaelAndriIngason@users.noreply.github.com>
2023-04-18 15:45:19 +0000

    404 page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+export default function Index() {
+    return (
+        <Layout title="404">
+            <div className="centered">
+                <h2>404 - Page Not Found</h2>
+                <Link href="/">Go back home</Link>
+            </div>
+        </Layout>
+    );
+}
+
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ export default function Index() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel lacus vitae sapien rutrum ornare ut nec odio. Nam et tellus sed ipsum imperdiet accumsan ac id eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sodales hendrerit massa, non tincidunt diam ullamcorper ac. Integer mauris nulla, blandit id libero at, tristique posuere felis. Vivamus sit amet fringilla ex. Duis vel neque lorem. Quisque imperdiet dapibus tortor a mollis. Nullam dictum pellentesque lacus at cursus. Sed sed sagittis nibh. Praesent faucibus mi faucibus tellus molestie luctus. Phasellus ut condimentum elit. Curabitur suscipit porta leo, vel venenatis lectus porta at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam vel lacus sed ligula gravida cursus. Aenean venenatis aliquam sem, at posuere mi scelerisque vitae.</p>
             <br />
             <p>Quisque faucibus arcu vitae pulvinar malesuada. Vestibulum consequat, lorem vitae dignissim ultricies, libero mi pharetra est, ac consectetur magna lacus et orci. Sed rhoncus libero eget mattis interdum. Curabitur vel commodo ex. Aliquam nec mauris pretium odio fringilla dictum. Ut sed facilisis erat, non facilisis ligula. Nulla nec ante pretium, tempor ante malesuada, iaculis sapien.</p>
+            <div className="images">
+                <img src="https://source.unsplash.com/IzmdWT2lW5Q" />
+                <img src="https://source.unsplash.com/ViEBSoZH6M4" />
+                <img src="https://source.unsplash.com/qDgTQOYk6B8" />
+            </div>
         </Layout>
     );
 }
